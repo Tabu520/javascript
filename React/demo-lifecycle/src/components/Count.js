7542de76af773108d397170bb2b66b217269b308
Tabu520 <thetai1122@gmail.com>
2021-08-13 22:53:06 +0700

    Functional state

--- React/demo-lifecycle/src/components/Count.js
+++ React/demo-lifecycle/src/components/Count.js
@@ -3,55 +3,64 @@ import React, { Component } from 'react'
 import './Count.css'
 
 class Count extends Component {
-    constructor(props) {
-        console.log('Counter constructor')
-        super(props)
-        this.state = {
-            count: 0
-        }
+  constructor(props) {
+    console.log('Counter constructor')
+    super(props)
+    this.state = {
+      count: 0
     }
+  }
 
-    increase() {
-        this.setState({
-            count: this.state.count + 1
-        });
-    }
+  increase() {
+    this.setState(state => {
+      return {
+        count: state.count + 1
+      }
+    });
+    this.setState(state => {
+      return {
+        count: state.count + 1
+      }
+    });
+  }
 
-    decrease() {
-        this.setState({
-            count: this.state.count - 1
-        })
-    }
+  decrease() {
+    this.setState(state => {
+      return {
+        count: state.count - 1
+      }
+    })
+  }
 
-    render() {
-        console.log('Counter render')
-        return (
-            <div className="count">
-                <button className="button" onClick={() => this.decrease()}>-</button>
-                <span> {this.state.count}</span>
-                <button className="button" onClick={() => this.increase()}>+</button>
-            </div>
-        )
-    }
+  render() {
+    console.log('Counter render')
+    return (
+      <div className="count">
+        <button className="button" onClick={() => this.decrease()}>-</button>
+        <span> {this.state.count}</span>
+        <button className="button" onClick={() => this.increase()}>+</button>
+      </div>
+    )
+  }
 
-    componentDidMount() {
-        console.log('Counter did mount')
-    }
+  componentDidMount() {
+    console.log('Counter did mount')
+  }
 
-    componentDidUpdate() {
-        console.log('Counter did update')
-    }
+  componentDidUpdate() {
+    console.log('Counter did update')
+  }
 
-    componentWillUnmount() {
-        console.log('Counter will unmount')
-    }
+  componentWillUnmount() {
+    console.log('Counter will unmount')
+  }
 
-    shouldComponentUpdate(nextProps, nextState) {
-        if(this.state.count === nextState.count) {
-            return false
-        }
-        return true
+  shouldComponentUpdate(nextProps, nextState) {
+    if (this.state.count === nextState.count) {
+      return false
     }
+    return true
+  }
 }
 
 export default Count
